d0e48949513904add24f59ebfef102624e31814b
faizfernanda <101172294+faizfernanda@users.noreply.github.com>
2023-12-17 23:59:09 +0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,119 @@
+<!-- start: Footer -->
+	<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: About -->
+				<div class="span3">
+					
+					<h3>Bu Nana</h3>
+					<p>
+                    Bu Nana merupakan tempat pemesanan catering secara online, melayani delivery dengan sistem pembayaran Cash On Delivery (COD) dan pembayaran via transfer bank
+					 </p>
+						
+				</div>
+				<!-- end: About -->
+
+				<!-- start: Photo Stream -->
+				<div class="span3">
+					
+					<h3>Alamat Kami</h3>
+                    Jl.Bumi Marina Emas No.69 Keputih, Sukolilo, Surabaya<br />
+                    Telp/SMS/WA : 082232480626<br />
+                    Website : <a href="http://www.bunana.com">www.bunana.com</a>
+                    </div>
+				<!-- end: Photo Stream -->
+
+				<div class="span6">
+				
+					<!-- start: Follow Us -->
+					<h3>Follow Us!</h3>
+					<ul class="social-grid">
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-twitter">
+											<a href="http://twitter.com"></a>
+										</div>
+										<div class="social-info-back social-twitter-hover">
+											<a href="http://twitter.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-linkedin">
+											<a href="http://facebook.com"></a>
+										</div>
+										<div class="social-info-back social-linkedin-hover">
+											<a href="http://facebook.com"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-dribbble">
+											<a href="http://dribble.com"></a>
+										</div>
+										<div class="social-info-back social-dribble-hover">
+											<a href="http://dribble.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-flickr">
+											<a href="http://flickr.com"></a>
+										</div>
+										<div class="social-info-back social-flickr-hover">
+											<a href="http://flickr.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				
+				</div>
+				
+			</div>
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+
+	<!-- start: Copyright -->
+	<div id="copyright">
+	
+		<!-- start: Container -->
+		<div class="container">
+		
+			<p>
+				Copyright &copy; <a href="http://bootstrapmaster.com" alt="Bootstrap Themes">Warung Bu Nana &copy; 2021 </a> Official Website
+			</p>
+	
+		</div>
+		<!-- end: Container  -->
+		
+	</div>	
+	<!-- end: Copyright -->
